18c2991b3b523af1f058523ee7da4b01aad9a653
MrFlava <thatelitemaili33t@gmail.com>
2026-06-03 19:27:34 +0300

    Operators

---
+++ operators.cpp
@@ -0,0 +1,23 @@
+# include <iostream>
+
+int main(){
+    int students = 30;
+
+    students += 5; // students = students + 5
+    std::cout << "After adding 5 students: " << students << std::endl;
+
+    students -= 3; // students = students - 3
+    std::cout << "After removing 3 students: " << students << std::endl;
+
+    students *= 2; // students = students * 2
+    std::cout << "After doubling the number of students: " << students << std::endl;
+
+    students /= 4; // students = students / 4
+    std::cout << "After dividing by 4: " << students << std::endl;
+
+
+    int remainder = students % 3; // remainder = students % 3
+    std::cout << "Remainder when students is divided by 3: " << remainder << std::endl;
+
+    return 0;
+}
