998cef7f7e693da7234664c438dc8b70b18341e0
MrFlava <thatelitemaili33t@gmail.com>
2026-07-05 14:04:52 +0300

    Lesson#20

---
+++ forLoops.cpp
@@ -0,0 +1,18 @@
+# include <iostream>
+
+int main(){
+    int number;
+
+    for(int i = 10; i <= 0; i-=2){
+        std::cout << i << std::endl;
+
+    //     std::cout << "Enter a positive number: ";
+    //     std::cin >> number;
+    //     if (number >= 0) {
+    //         break;
+    //     }
+    }
+    std::cout << "Happy Birthday!" << std::endl;
+    // std::cout << "You entered: " << number << std::endl;
+    return 0;
+}
